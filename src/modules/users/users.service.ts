1a3f788bf1e3096fa3f4111e17ba9298ab76647c
Henrique Martins Alexandre <developer.henrique1373@gmail.com>
2024-11-23 14:44:45 -0300

    feat: add method to get users by id

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersRepo: UsersRepository) {}
+
+  async getUserById(userId: string) {
+    const user = await this.usersRepo.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return {
+      name: user.name,
+      email: user.email,
+    };
+  }
+}
